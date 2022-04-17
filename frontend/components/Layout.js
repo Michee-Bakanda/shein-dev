b1a7b000011c3001d51473d7ad3be99df51ecb73
schadrack <schadrackbotombe@gmail.com>
2022-04-17 16:53:58 +0200

    created layout page

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './navbar/Navbar'
 
 const Layout = ({children}) => {
   return (
   <>
-    <Navba/>
-
+    <Navbar/>
+    {children}
   
   </>
   )
