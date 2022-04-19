ef3a890e8802aa7624c7d4760e5d67458aff9457
schadrack <schadrackbotombe@gmail.com>
2022-04-19 13:50:59 +0200

    added login page

---
+++ frontend/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div style={{background:'#fff',border:'1px solid red',height:'200px'}}>
+
+       <h1 style={{margin:'0'}}>Login here</h1>
+
+    </div>
+  )
+}
+
+export default Login
