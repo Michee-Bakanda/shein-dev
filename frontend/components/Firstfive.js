867e80b4dc476d2a34dac4758d27133865b7b3d3
xolani <xolanilingela07@gmail.com>
2022-04-21 20:25:46 +0200

    done

--- frontend/components/Firstfive.js
+++ frontend/components/Firstfive.js
@@ -7,7 +7,11 @@ const Product= () => {
                 <img src="first-five2.webp" style={{width:"95%"}}/>
                 <img src="firstfive-word1.webp" style={{width:"95%"}}/>
                 <img src="first-five3.webp" style={{width:"95%"}}/>
-                <img src="firstfive-word2.webp" style={{width:"95%"}}/>
+                <img src="1622459010b9873040cab74fa20fa39b5b6872dfab.webp" style={{width:"95%"}}/>
+                <img src="162245905521c474b4510fed7d217b680881477042.webp" style={{width:"95%"}}/>
+                <img src="162245906946fb5e1916d2aaf44c8f60d811f02a7e.webp" style={{width:"95%"}}/>
+                <img src="1622459081a8698b561e85b2de04f34a4fad184fbc.webp" style={{width:"95%"}}/>
+                <img src="16224590925636e8a4db70c88ee73dfb3d96e17e3b.webp" style={{width:"95%"}}/>
             </div>
 
         </div>
