95d2e026c20bc0e9f33a09126c017c63ba2595e3
michee <micheebakanda@gmail.com>
2022-05-04 15:01:12 +0200

    add the backend code

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
-const express = require("express")
-require('dotenv').config()
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
-app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}`)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+
+app.listen(3000, () => {
+  console.log("Backend server is running!");
+});
+
 
